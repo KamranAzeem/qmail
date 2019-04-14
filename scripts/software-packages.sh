9baa9eb38d7bfa99b4cb0821475e62a9949ab13a
Kamran Azeem <kaz@praqma.net>
2019-04-14 19:31:58 +0200

    added EPEL and some packages from EPEL

--- scripts/software-packages.sh
+++ scripts/software-packages.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-echo "This script installs all necessary packages required for qmail setup."
+echo "This script installs all necessary packages required for qmail setup. - on CENTOS 7" 
+
+yum -y install epel-release*
 
 yum -y install  kernel-devel kernel-headers git \
   net-snmp net-snmp-utils net-snmp-libs mrtg \
   httpd httpd-devel php php-imap php-mysql php-gd php-pear php-zlib php-mbstring php-xml \
   gcc gcc-c++ gdbm-devel pcre-devel libtool-ltdl libtool-ltdl-devel \
-  mysql-server mysql-devel db4 db4-devel postgresql-devel \
+  mariadb-server mariadb-devel mariadb libdb4 libdb4-devel libdb4-utils compat-db47 postgresql-devel \
   openssl openssl-devel openldap-servers \
   perl perl-libwww-perl perl-Digest-SHA1 perl-Digest-HMAC perl-Net-DNS perl-HTML-Tagset perl-HTML-Parser perl-Time-HiRes perl-TimeDate perl-suidperl perl-DateManip \
   spamassassin expect zlib-devel \
   fam fam-devel gamin-devel patch patchutils
+
+# No package php-imap available. # in epel
+# No package db4 available. # in epel - libdb4
+# No package db4-devel available. # in epel - libdb4-devel
+
 
